71cc26aeff7db7447dd51fe29c43b46f3e9e964d
North Krimsly <north@highintegritydesign.com>
2013-11-17 20:31:07 -0800

    Initial add of hid-cta-button php code.

---
+++ hid-cta-button.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: hid-cta-button
+Plugin URI: http://highintegritydesign.com
+Description: Provide a call-to-action button using a shortcode
+Version: 1.0
+Author: North Krimsly
+Author URI: http://highintegritydesign.com
+License: GPL2
+
+hid-cta-button is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+hid-cta-button is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with hid-cta-button. If not, see http://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+class HID_cta_button {  
+    public function __construct()  
+    {  
+        // attach our 'do' function to the new shortcode. This will automatically
+        // work within pages and posts, so we don't need to hook into any additional
+        // action or filter hooks.
+        add_shortcode('hid-cta-button', array($this, 'do_cta_shortcode'));  
+    }  
+
+    public function do_cta_shortcode($atts)  
+    {  
+        // extract the shortcode attributes into variables and give default values
+        // The default class is 'hid-cta-button' so it will hook into css for that.
+        extract(shortcode_atts(array(  
+            'text' => "Button",  
+            'classes' => "hid-cta-button",  
+            'url' => "http://wordpress.org"  
+        ), $atts));  
+
+        // construct a link using the URL, classes and button text supplied 
+        // in the shortcode attributes or from the default values
+        $html = "<a href='{$url}' class='{$classes}'>{$text}</a>";
+        return $html;
+    }  
+}
+
+// construct a new instance of the cta button
+$hid_cta_button_instance = new HID_cta_button();  
+
+?>
